254a87fbf2726441c215bd549bca59ad507c97c6
itc-s25003 <s25003@std.it-college.ac.jp>
2025-05-20 06:06:59 +0000

    challenge3

---
+++ Chapter03/challenge3-02.py
@@ -0,0 +1,5 @@
+keion = 9
+if keion < 10:
+    print("けいおんが10未満です。")
+else:
+    print("けいおんが10以上です。")
---
+++ Chapter03/challenge3-03.py
@@ -0,0 +1,7 @@
+keion = 26
+if keion <= 10:
+    print("けいおんが10以下です。")
+elif keion <= 25:
+    print("けいおんが10より大きく25以下です。")
+else:
+    print("けいおんが25より大きいです。")
