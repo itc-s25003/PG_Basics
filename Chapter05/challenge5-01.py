e1dd8a5b684e5066dfabe15c1ceb095331181db6
itc-s25003 <s25003@std.it-college.ac.jp>
2025-06-02 06:16:57 +0000

    challenge5-01 ~ challenge05-06

---
+++ Chapter05/challenge5-01.py
@@ -0,0 +1,9 @@
+bandList = ["my dead girlfriend",
+            "Tokenainamae",
+            "Forsaken Autumn",
+            "Lily Chou-Chou",
+            "kinoue64",
+            "Kinokoteikoku",
+            "Plastic Girl In Closet"]
+
+print(bandList)
---
+++ Chapter05/challenge5-06.py
@@ -0,0 +1,5 @@
+li = [1, 1, 2, 3, 3, 4, 3 ,5, 5]
+s = set(li)
+
+print(li)
+print(s)
